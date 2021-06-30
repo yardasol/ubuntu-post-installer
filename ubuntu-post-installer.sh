a3c600aa1da3f006ce2d7e3bdf9069b5859ede87
yardasol <oyardas2@illinos.edu>
2021-06-30 13:00:53 -0500

    add terminal setup to script

--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -12,7 +12,8 @@ sudo apt update
 echo "parse_git_branch() {" >> $BRC
 echo "  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'" >> $BRC
 echo "}" >> $BRC
-echo "export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(parse_git_branch)\[\033[00m\]$ "" >> $BRC
+echo "" >> $BRC
+echo 'export PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(parse_git_branch)\[\033[00m\]$ "' >> $BRC
 
 # install Write
 wget -O - http://www.styluslabs.com/download/write-tgz | tar -C $HOME -xJ
